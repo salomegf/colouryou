3edec01bbcfc145d9eb2d74a3185242cc9922676
Salomé Gfeller <salomegf.sg@gmail.com>
2022-10-31 11:30:33 +0100

    essais users

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,34 @@
 import express from "express";
+import User from "../models/user.js";
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
+/* router.get("/", function (req, res, next) {
   res.send("Got a response from the users route");
+}); */
+
+/* router.get("/", authenticate, function (req, res, next) {
+  User.find().sort('name').exec(function(err, users) {
+    if (err) {
+      return next(err);
+    }
+
+    res.send(users);
+  });
+}); */
+
+router.post('/', function(req, res, next) {
+  // Create a new document from the JSON in the request body
+  const newUser = new User(req.body);
+
+  // Save that document
+  newUser.save(function(err, savedUser) {
+    if (err) {
+      return next(err);
+    }
+
+    // Send the saved document in the response
+    res.send(savedUser);
+  });
 });
 
 export default router;
