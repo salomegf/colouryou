f43fef4a1ec5aa7a9239c24a345e5c83fec66d1e
Salomé Gfeller <salomegf.sg@gmail.com>
2022-11-10 11:45:47 +0100

    geojson

--- models/photo.js
+++ models/photo.js
@@ -10,10 +10,10 @@ const photoSchema = new Schema({
     type: Date,
     default: Date.now
   },
-  location: {
+  /* location: {
     latitute: Number,
     longitude: Number,
-  },
+  }, */
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User'
@@ -25,9 +25,43 @@ const photoSchema = new Schema({
   url: {
     type: String,
     required: true,
+  },
+  location: {
+    type: {
+      type: String,
+      required: true,
+      enum: ['Point']
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      validate: {
+        validator: validateGeoJsonCoordinates,
+        message: '{VALUE} is not a valid longitude/latitude(/altitude) coordinates array'
+      }
+    }
   }
 }, {
   timestamps: true
 });
+
+
+// Create a geospatial index on the location property.
+photoSchema.index({
+  location: '2dsphere'
+});
+
+// Validate a GeoJSON coordinates array (longitude, latitude and optional altitude).
+function validateGeoJsonCoordinates(value) {
+  return Array.isArray(value) && value.length >= 2 && value.length <= 3 && isLongitude(value[0]) && isLatitude(value[1]);
+}
+
+function isLatitude(value) {
+  return value >= -90 && value <= 90;
+}
+
+function isLongitude(value) {
+  return value >= -180 && value <= 180;
+}
 
 export default mongoose.model('Photo', photoSchema)
